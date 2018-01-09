42b2b49e5adda2f99cb99ec01ca24d24a0de8f3d
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2018-01-09 13:27:31 -0800

    Complete palindrome permutation

--- arrays_strings/interview_questions.js
+++ arrays_strings/interview_questions.js
@@ -22,7 +22,7 @@ function isUnique(string) {
 // Another is similar to below but have only one hash that counts frequency
 // of first string. Then in another iteration (for the second string) decrement
 // the count in the one hash. If the value drops below zero, for sure it is
-// not a permutation. 
+// not a permutation.
 
 // Initial Implementation:
 function checkPermutation(stringOne, stringTwo) {
@@ -52,4 +52,54 @@ function checkPermutation(stringOne, stringTwo) {
   }
 
   return true;
+}
+
+// 1.3 URLify: Write a method to replace all spaces in a string with a '%20'.
+// You may assume that the string has sufficient space at the end to hold the
+// additional characters, and that you are given the 'true' length of the
+// string.
+// (Note: If implementing in Java, please use a character array so that you can
+// perform this operation in place.)
+// Example:
+// Input: "Mr John Smith    ", 13
+// Output: "Mr%20John%20Smith"
+
+// Initial Implementation:
+function URLify(string, length) {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    if (string[i] === ' ' && string[i+1] !== ' ') {
+      result += '%20';
+    } else {
+      result += string[i];
+    }
+  }
+  return result;
+}
+// Revisit
+
+// Palindrome Permutation: Given a string, write a function to check if it
+// is a permutation of a palindrome. A palindrome is a word or phrase that is
+// the same forwards and backwards. A permutation is a rearrangment of letters.
+// The palindrome does not need to be limited to just dictionary words.
+// Example:
+// Input: Tact Coa
+// Output: True (permutations: 'taco cat', 'atco cta', etc.)
+
+// Initial Implementation:
+function palindromePerm(string) {
+  let history = {};
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') continue;
+
+    if (history[string[i].toLowerCase()]) {
+      delete history[string[i].toLowerCase()];
+    } else {
+      history[string[i].toLowerCase()] = true;
+    }
+  }
+
+  if (Object.keys(history).length < 2) return true;
+  return false;
 }
